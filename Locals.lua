9994324a474b465d947ad114fc5e59c4605f5665
eXochron <exochron@gamerstate.de>
2023-11-11 10:49:34 +0100

    feat(local) added remaining languages

--- Locals.lua
+++ Locals.lua
@@ -43,9 +43,15 @@ if locale == "deDE" then
 elseif locale == "esES" then
     --@localization(locale="esES", format="lua_additive_table", handle-unlocalized=comment)@
     --@localization(locale="esES", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
+elseif locale == "esMX" then
+    --@localization(locale="esMX", format="lua_additive_table", handle-unlocalized=comment)@
+    --@localization(locale="esMX", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
 elseif locale == "frFR" then
     --@localization(locale="frFR", format="lua_additive_table", handle-unlocalized=comment)@
     --@localization(locale="frFR", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
+elseif locale == "itIT" then
+    --@localization(locale="itIT", format="lua_additive_table", handle-unlocalized=comment)@
+    --@localization(locale="itIT", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
 elseif locale == "koKR" then
     --@localization(locale="koKR", format="lua_additive_table", handle-unlocalized=comment)@
     --@localization(locale="koKR", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
@@ -55,4 +61,10 @@ elseif locale == "ptBR" then
 elseif locale == "ruRU" then
     --@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized=comment)@
     --@localization(locale="ruRU", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
+elseif locale == "zhCN" then
+    --@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized=comment)@
+    --@localization(locale="zhCN", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
+elseif locale == "zhTW" then
+    --@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized=comment)@
+    --@localization(locale="zhTW", namespace="Settings", format="lua_additive_table", handle-unlocalized=comment)@
 end
