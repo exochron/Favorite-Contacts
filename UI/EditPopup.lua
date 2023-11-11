23fb518e3e242f47d79c0bd198a7a988053d0a18
eXochron <exochron@gamerstate.de>
2023-11-11 10:43:53 +0100

    feat(ui): restyled add/edit contact frame to fit modern styling

--- UI/EditPopup.lua
+++ UI/EditPopup.lua
@@ -5,35 +5,43 @@ local CONTACT_DEFAULT_ICON = "INV_Misc_GroupLooking"
 local popup, buttonContainer, activeContactIndex
 
 local function CreateWindow()
-    local frame = CreateFrame("Frame", "FavoriteContactsEditFrame", UIParent)
+    local AceGUI = LibStub("AceGUI-3.0")
+
+    local frame = CreateFrame("Frame", "FavoriteContactsEditFrame", UIParent, "ButtonFrameTemplate")
     tinsert(UISpecialFrames, frame:GetName()) -- also close frame on escape
 
-    frame:EnableMouse(true)
     frame:SetWidth(419)
-    frame:SetHeight((min(ceil(#ADDON.iconFiles / 8), 8) * 45) + 132)
-    frame:SetPoint("CENTER")
+    frame:SetHeight((min(ceil(#ADDON.iconFiles / 8), 8) * 45) + 191)
+    frame:SetPoint("TOPLEFT", MailFrame, "TOPRIGHT", MailFrame:GetAttribute("UIPanelLayout-extraWidth"), 0)
     frame:SetFrameStrata("DIALOG")
 
-    local tex = frame:CreateTexture(nil, "BACKGROUND")
-    tex:SetPoint("TOPLEFT", 7, -7)
-    tex:SetPoint("BOTTOMRIGHT", -7, 7)
-    tex:SetColorTexture(0, 0, 0, 0.8)
+    frame.ScrollFrame = CreateFrame("ScrollFrame", nil, frame, "ScrollFrameTemplate")
+    frame.ScrollFrame.ScrollBar:SetPoint("TOPLEFT", frame.ScrollFrame, "TOPRIGHT", SCROLL_FRAME_SCROLL_BAR_OFFSET_LEFT, 0);
+    frame.ScrollFrame.ScrollBar:SetPoint("BOTTOMLEFT", frame.ScrollFrame, "BOTTOMRIGHT", SCROLL_FRAME_SCROLL_BAR_OFFSET_LEFT, 0);
 
-    frame.BorderBox = CreateFrame("Frame", nil, frame, "SelectionFrameTemplate")
-    frame.BorderBox:SetPoint("TOPLEFT")
-    frame.BorderBox:SetPoint("BOTTOMRIGHT")
+    frame.CancelButton = AceGUI:Create("Button")
+    frame.CancelButton:SetAutoWidth(true)
+    frame.CancelButton:SetText(CANCEL)
+    frame.CancelButton:SetParent({ content = frame })
+    frame.CancelButton:SetPoint("BOTTOMRIGHT", -6, 4)
+    frame.CancelButton:SetCallback("OnClick", function() frame:Hide() end)
+    frame.CancelButton.frame:Show()
+
+    frame.SaveButton = AceGUI:Create("Button")
+    frame.SaveButton:SetAutoWidth(true)
+    frame.SaveButton:SetText(SAVE)
+    frame.SaveButton:SetParent({ content = frame })
+    frame.SaveButton:SetPoint("RIGHT", frame.CancelButton.frame, "LEFT", -2, 0)
+    frame.SaveButton.frame:Show()
 
     -- ElvUI hack
     if InboxFrame.backdrop and ElvUI then
         frame:StripTextures()
         frame:CreateBackdrop('Transparent')
         frame.backdrop:SetAllPoints()
-        frame.BorderBox:StripTextures()
 
-        local E, _ = unpack(ElvUI)
-        local S = E:GetModule('Skins')
-        S:HandleButton(frame.BorderBox.OkayButton, true)
-        S:HandleButton(frame.BorderBox.CancelButton, true)
+        local portrait = frame.portrait or frame:GetPortrait()
+        portrait:Hide()
     end
 
     return frame
@@ -43,6 +51,7 @@ local function UpdateForm(container, iconText)
     for _, widget in pairs(container.children) do
         widget:SetChecked(iconText == widget:GetUserData("icon"))
     end
+    ADDON:SetTexture(popup.portrait or popup:GetPortrait(), iconText)
 end
 
 local function CreateIconButtons(AceGUI, container)
@@ -52,7 +61,7 @@ local function CreateIconButtons(AceGUI, container)
 
         button:SetUserData("icon", icon)
         button:SetCallback("OnClick", function()
-            popup.BorderBox.IconName:SetText(icon)
+            popup.IconName:SetText(icon)
             UpdateForm(container, icon)
         end)
 
@@ -86,19 +95,29 @@ local function CreateForm()
 
     local window = CreateWindow()
 
-    window.BorderBox.ContactNameEditBox = CreateEditBox(AceGUI, L["Contact Name:"], window.BorderBox)
-    window.BorderBox.ContactNameEditBox:SetPoint("TOPLEFT", 24, -14)
-    window.BorderBox.NoteEditBox = CreateEditBox(AceGUI, L["Contact Note:"], window.BorderBox)
-    window.BorderBox.NoteEditBox:SetPoint("TOPLEFT", 24, -36)
-    window.BorderBox.IconName = CreateEditBox(AceGUI, MACRO_POPUP_CHOOSE_ICON, window.BorderBox)
-    window.BorderBox.IconName:SetPoint("TOPLEFT", 24, -70)
+    window.ContactNameEditBox = CreateEditBox(AceGUI, L["Contact Name:"], window)
+    window.ContactNameEditBox:SetPoint("TOPLEFT", 24, -70)
+    window.NoteEditBox = CreateEditBox(AceGUI, L["Contact Note:"], window)
+    window.NoteEditBox:SetPoint("TOPLEFT", window.ContactNameEditBox.frame, "BOTTOMLEFT", 0, 0)
 
-    buttonContainer = AceGUI:Create("ScrollFrame")
-    buttonContainer:SetParent({ content = window.BorderBox })
-    buttonContainer:SetPoint("TOPLEFT", 24, -95)
-    buttonContainer:SetPoint("BOTTOMRIGHT", -24, 35)
+    local divider = window:CreateTexture(nil, "ARTWORK")
+    divider:SetAtlas("Options_HorizontalDivider", true)
+    divider:SetPoint("CENTER", window.NoteEditBox.frame, "BOTTOM", 0, -7)
+    divider:SetWidth(400)
+
+    window.IconName = CreateEditBox(AceGUI, MACRO_POPUP_CHOOSE_ICON, window)
+    window.IconName:SetPoint("TOPLEFT", window.NoteEditBox.frame, "BOTTOMLEFT", 0, -14)
+
+    window.ScrollFrame:SetPoint("TOPLEFT",  window.IconName.frame, "BOTTOMLEFT", 0, -3)
+    window.ScrollFrame:SetPoint("BOTTOMRIGHT", -44, 35)
+
+    buttonContainer = AceGUI:Create("SimpleGroup")
+    buttonContainer:SetParent({ content = window.ScrollFrame })
+    buttonContainer:SetPoint("TOPLEFT")
+    buttonContainer:SetWidth(360)
     buttonContainer:SetLayout("Flow")
     buttonContainer.frame:Show()
+    window.ScrollFrame:SetScrollChild(buttonContainer.frame)
 
     CreateIconButtons(AceGUI, buttonContainer)
 
@@ -109,18 +128,17 @@ local function CreateForm()
         ADDON:SetEnableContacts(true)
     end)
 
-    window.BorderBox.OnCancel = function() window:Hide() end
-    window.BorderBox.OnOkay = function()
+    window.SaveButton:SetCallback("OnClick", function()
         window:Hide()
-        local recipient = window.BorderBox.ContactNameEditBox:GetText()
+        local recipient = window.ContactNameEditBox:GetText()
         if recipient ~= "" then
-            local icon = window.BorderBox.IconName:GetText()
+            local icon = window.IconName:GetText()
             if not icon or string.len(icon) == 0 then
                 icon = CONTACT_DEFAULT_ICON
             end
-            ADDON:SetContact(activeContactIndex, recipient, icon, window.BorderBox.NoteEditBox:GetText())
+            ADDON:SetContact(activeContactIndex, recipient, icon, window.NoteEditBox:GetText())
         end
-    end
+    end)
 
     return window
 end
@@ -134,13 +152,15 @@ function ADDON:ShowEditContactPopup(index)
 
     local contact = self.settings.contacts[index] or {}
 
-    local nameEdit = popup.BorderBox.ContactNameEditBox
+    popup:SetTitle(contact.recipient and ADDON.L.EDIT_CONTACT_TITLE or ADDON.L.CREATE_CONTACT_TITLE)
+
+    local nameEdit = popup.ContactNameEditBox
     nameEdit:SetText(contact.recipient or "")
 
-    popup.BorderBox.NoteEditBox:SetText(contact.note or "")
+    popup.NoteEditBox:SetText(contact.note or "")
 
     local icon = contact.icon or CONTACT_DEFAULT_ICON
-    popup.BorderBox.IconName:SetText(icon)
+    popup.IconName:SetText(icon)
 
     UpdateForm(buttonContainer, icon)
     popup:Show()
