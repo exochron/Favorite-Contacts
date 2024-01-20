00afb0ded022338bcc0c957ecb88dcb8aeae5d03
eXochron <exochron@gamerstate.de>
2024-01-20 11:26:15 +0100

    feat: added icons of all class specialisation [(cf#15)[https://legacy.curseforge.com/wow/addons/favorite-contacts/issues/15]]

--- Icons.retail.lua
+++ Icons.retail.lua
@@ -13,6 +13,21 @@ ADDON.iconFiles = {
     "ClassIcon_Mage", "ClassIcon_Monk", "ClassIcon_Paladin", "ClassIcon_Priest", "ClassIcon_Rogue", "ClassIcon_Shaman",
     "ClassIcon_Warlock", "ClassIcon_Warrior",
 
+    --specialisations
+    "Spell_Deathknight_BloodPresence", "Spell_Deathknight_FrostPresence","Spell_Deathknight_UnholyPresence",
+    "Ability_DemonHunter_SpecDPS","Ability_DemonHunter_SpecTank",
+    "Spell_Nature_StarFall","Ability_druid_catform","Ability_racial_bearform","Spell_nature_healingtouch",
+    "Classicon_evoker_devastation","Classicon_evoker_preservation",
+    "Ability_hunter_bestialdiscipline","Ability_hunter_focusedaim","Ability_hunter_camouflage",
+    "Spell_holy_magicalsentry","Spell_fire_firebolt02","Spell_frost_frostbolt02",
+    "Spell_monk_brewmaster_spec", "Spell_monk_mistweaver_spec", "Spell_monk_windwalker_spec",
+    "Spell_holy_holybolt","Ability_paladin_shieldofthetemplar","Spell_holy_auraoflight",
+    "Spell_holy_powerwordshield","Spell_holy_guardianspirit","Spell_shadow_shadowwordpain",
+    "Ability_rogue_deadlybrew", "Inv_sword_30", "Ability_stealth",
+    "Spell_nature_lightning", "Spell_shaman_improvedstormstrike", "Spell_nature_magicimmunity",
+    "Spell_shadow_deathcoil", "Spell_shadow_metamorphosis", "Spell_shadow_rainoffire",
+    "Ability_warrior_savageblow", "Ability_warrior_innerrage", "Ability_warrior_defensivestance",
+
     -- people - uses texture atlas instead of plain textures
     "raceicon-human-female", "raceicon-human-male",
     "raceicon-gnome-female", "raceicon-gnome-male",
