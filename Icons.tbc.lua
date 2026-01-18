6a93339d00902db86ea5e862cea508580dc64be3
eXochron <exochron@gamerstate.de>
2026-01-18 21:15:08 +0100

    feat: added support for TBC

--- Icons.tbc.lua
+++ Icons.tbc.lua
@@ -4,20 +4,18 @@ ADDON.iconFiles = {
     "INV_Misc_GroupLooking",
 
     -- class
-    "ClassIcon_DeathKnight", "ClassIcon_Druid", "ClassIcon_Hunter",
-    "ClassIcon_Mage", "ClassIcon_Monk", "ClassIcon_Paladin", "ClassIcon_Priest", "ClassIcon_Rogue", "ClassIcon_Shaman",
+    "ClassIcon_Druid", "ClassIcon_Hunter",
+    "ClassIcon_Mage", "ClassIcon_Paladin", "ClassIcon_Priest", "ClassIcon_Rogue", "ClassIcon_Shaman",
     "ClassIcon_Warlock", "ClassIcon_Warrior",
 
     --specialisations
-    "Spell_Deathknight_BloodPresence", "Spell_Deathknight_FrostPresence","Spell_Deathknight_UnholyPresence",
     "Spell_Nature_StarFall","Ability_druid_catform","Ability_racial_bearform","Spell_nature_healingtouch",
-    "Ability_hunter_bestialdiscipline","Ability_hunter_focusedaim","Ability_hunter_camouflage",
+    "Ability_hunter_beasttaming","Ability_marksmanship","Ability_hunter_swiftstrike",
     "Spell_holy_magicalsentry","Spell_fire_firebolt02","Spell_frost_frostbolt02",
-    "Spell_monk_brewmaster_spec", "Spell_monk_mistweaver_spec", "Spell_monk_windwalker_spec",
-    "Spell_holy_holybolt","Ability_paladin_shieldofthetemplar","Spell_holy_auraoflight",
+    "Spell_holy_holybolt","Spell_holy_devotionaura","Spell_holy_auraoflight",
     "Spell_holy_powerwordshield","Spell_holy_guardianspirit","Spell_shadow_shadowwordpain",
-    "Ability_rogue_deadlybrew", "Inv_sword_30", "Ability_stealth",
-    "Spell_nature_lightning", "Spell_shaman_improvedstormstrike", "Spell_nature_magicimmunity",
+    "Ability_rogue_eviscerate", "Ability_backstab", "Ability_stealth",
+    "Spell_nature_lightning", "Spell_nature_lightningshield", "Spell_nature_magicimmunity",
     "Spell_shadow_deathcoil", "Spell_shadow_metamorphosis", "Spell_shadow_rainoffire",
     "Ability_warrior_savageblow", "Ability_warrior_innerrage", "Ability_warrior_defensivestance",
 
@@ -27,21 +25,17 @@ ADDON.iconFiles = {
     "raceicon-dwarf-female", "raceicon-dwarf-male",
     "raceicon-nightelf-female", "raceicon-nightelf-male",
     "raceicon-draenei-female", "raceicon-draenei-male",
-    "raceicon-worgen-female", "raceicon-worgen-male",
-    "raceicon-pandaren-female", "raceicon-pandaren-male",
     "raceicon-orc-female", "raceicon-orc-male",
     "raceicon-tauren-female", "raceicon-tauren-male",
     "raceicon-troll-female", "raceicon-troll-male",
     "raceicon-undead-female", "raceicon-undead-male",
     "raceicon-bloodelf-female", "raceicon-bloodelf-male",
-    "raceicon-goblin-female", "raceicon-goblin-male",
 
     --profession
     "inv_misc_gem_01",
     "Trade_Engraving",
     "Trade_Engineering",
     "Trade_Alchemy",
-    "inv_inscription_tradeskill01",
     "Trade_Tailoring",
     "inv_misc_armorkit_17",
     "Trade_BlackSmithing",
@@ -51,10 +45,8 @@ ADDON.iconFiles = {
     "inv_misc_food_15",
     "Trade_Fishing",
     "spell_holy_sealofsacrifice", -- first aid
-    "trade_archaeology",
 
     -- faction
-    "ACHIEVEMENT_GUILDPERK_MOBILEBANKING",
     "INV_BannerPVP_01",
     "INV_BannerPVP_02",
 
